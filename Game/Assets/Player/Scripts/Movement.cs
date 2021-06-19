3856062d9e0f9cc642fc5452309da1ebce5430fa
LAPTOP-URRDHECG\Margo <margoking1966@hotmail.com>
2021-06-19 17:45:43 -0400

    1st Version
    This version has movement of the player and some animation

---
+++ Game/Assets/Player/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    bool jump = false;
+    bool charge = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Charge")) 
+        {
+            charge = true;
+        } else if (Input.GetButtonUp("Charge")) 
+        {
+            charge = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, charge, jump);
+        jump = false;
+    }
+}
